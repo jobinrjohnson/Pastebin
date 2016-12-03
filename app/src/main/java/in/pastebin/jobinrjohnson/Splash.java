f3f9db1812858bbc7e68aaa4994d88e83d8cb189
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-03 08:54:28 +0530

    implemented splash functionality

--- app/src/main/java/in/pastebin/jobinrjohnson/Splash.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/Splash.java
@@ -1,5 +1,6 @@
 package in.pastebin.jobinrjohnson;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,25 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(1000);
+
+                    //
+                    //Thread sleapt for 1000 increase later
+                    //
+
+                    startActivity(new Intent());
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
     }
 }
