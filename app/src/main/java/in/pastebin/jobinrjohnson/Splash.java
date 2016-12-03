340c7728662b3aabcfd98a2f92d4dccc288d4531
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-03 08:58:04 +0530

    added main activity

--- app/src/main/java/in/pastebin/jobinrjohnson/Splash.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/Splash.java
@@ -1,8 +1,8 @@
 package in.pastebin.jobinrjohnson;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
 
@@ -22,7 +22,7 @@ public class Splash extends AppCompatActivity {
                     //Thread sleapt for 1000 increase later
                     //
 
-                    startActivity(new Intent());
+                    startActivity(new Intent(Splash.this, MainActivity.class));
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
