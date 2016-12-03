40a54620c1f6b0b4b03727cb32ed9c1358909442
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-03 21:53:56 +0530

    added functions

--- app/src/main/java/in/pastebin/jobinrjohnson/PastebinRequest.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/PastebinRequest.java
@@ -1,8 +1,15 @@
 package in.pastebin.jobinrjohnson;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by jobin on 12/3/2016.
@@ -13,6 +20,44 @@ public class PastebinRequest {
 
     public PastebinRequest(URL url) throws IOException {
         conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(15000);
+        conn.setConnectTimeout(15000);
+        conn.setRequestMethod("POST");
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+    }
+
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
+    }
+
+
+    public void postData(HashMap postData) throws IOException {
+
+
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(os, "UTF-8"));
+        writer.write(getPostDataString(postData));
+
+        writer.flush();
+        writer.close();
+        os.close();
+        int responseCode = conn.getResponseCode();
+
+
     }
 
 }
