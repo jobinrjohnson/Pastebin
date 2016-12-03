ddf3c3588f5d243f4f9b5636a8d4bca67890c78d
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-03 13:29:01 +0530

    addd some functionality

--- app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
@@ -1,5 +1,6 @@
 package in.pastebin.jobinrjohnson;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,13 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddPaste extends AppCompatActivity {
 
     LinearLayout llFirstStep, ll3rdStep;
-    Button btnProceed1, btnProceed2;
-    EditText etPasteName;
+    Button btnProceed1, btnProceed3;
+    EditText etPasteName, etPasteText;
     Spinner spPastePrivacy;
+    String name, privacy, pasteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +43,11 @@ public class AddPaste extends AppCompatActivity {
         ll3rdStep = (LinearLayout) findViewById(R.id.ll3rdStep);
 
         etPasteName = (EditText) findViewById(R.id.etPasteName);
+        etPasteText = (EditText) findViewById(R.id.etPastetext);
         spPastePrivacy = (Spinner) findViewById(R.id.spPastePrivacy);
 
         btnProceed1 = (Button) findViewById(R.id.btnProceed1);
+        btnProceed3 = (Button) findViewById(R.id.btnProceed3);
 
         btnProceed1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +56,81 @@ public class AddPaste extends AppCompatActivity {
                 ll3rdStep.setVisibility(View.VISIBLE);
             }
         });
+
+        btnProceed3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomePost();
+            }
+        });
+
+    }
+
+    private void doSomePost() {
+        String url = getResources().getString(R.string.api_url) + "api_post.php";
+        name = etPasteName.getText().toString();
+        privacy = spPastePrivacy.getSelectedItemPosition() + "";
+        new ServerPaste().execute(url);
+    }
+
+
+    private class ServerPaste extends AsyncTask<String, Void, String> {
+
+        Map<String, String> postData;
+        String dataReturned;
+        boolean status = false;
+        int type;
+
+        ServerPaste(int type) {
+            this.type = type;
+        }
+
+        ServerPaste() {
+            type = 0;
+        }
+
+        public Map<String, String> getPasteData() {
+            Map<String, String> data = new HashMap<>();
+            data.put("api_dev_key", getResources().getString(R.string.api_key));
+            data.put("api_option", "paste");
+            data.put("api_paste_name", name);
+            data.put("api_paste_private", privacy);
+            data.put("api_paste_code", "");
+            return data;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpRequest request = HttpRequest.post(params[0], postData, true);
+            if (request.ok()) {
+                status = true;
+                dataReturned = request.body();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            switch (type) {
+                case 0:
+                    postData = getPasteData();
+                    break;
+                default:
+                    postData = new HashMap<>();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (status) {
+                Toast.makeText(AddPaste.this, dataReturned, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(AddPaste.this, "Nothing returned", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
