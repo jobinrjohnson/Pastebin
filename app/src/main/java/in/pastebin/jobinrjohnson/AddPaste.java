7ed9e38c0b0d9182fdd0ed9a3bdb32799b09d3d9
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-03 13:03:29 +0530

    addd some functionality

--- app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
@@ -1,13 +1,20 @@
 package in.pastebin.jobinrjohnson;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 public class AddPaste extends AppCompatActivity {
+
+    LinearLayout llFirstStep, ll3rdStep;
+    Button btnProceed1, btnProceed2;
+    EditText etPasteName;
+    Spinner spPastePrivacy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +23,31 @@ public class AddPaste extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        llFirstStep = (LinearLayout) findViewById(R.id.llFirstStep);
+        ll3rdStep = (LinearLayout) findViewById(R.id.ll3rdStep);
+
+        etPasteName = (EditText) findViewById(R.id.etPasteName);
+        spPastePrivacy = (Spinner) findViewById(R.id.spPastePrivacy);
+
+        btnProceed1 = (Button) findViewById(R.id.btnProceed1);
+
+        btnProceed1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                llFirstStep.setVisibility(View.GONE);
+                ll3rdStep.setVisibility(View.VISIBLE);
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
