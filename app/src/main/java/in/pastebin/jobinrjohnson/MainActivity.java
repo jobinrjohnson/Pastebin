a879367ade7f120b6204a744f0df5f27f08d8de2
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-03 09:06:11 +0530

    added add activity

--- app/src/main/java/in/pastebin/jobinrjohnson/MainActivity.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/MainActivity.java
@@ -1,9 +1,9 @@
 package in.pastebin.jobinrjohnson;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -27,8 +27,11 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                startActivity(new Intent(MainActivity.this, AddPaste.class));
+
             }
         });
 
