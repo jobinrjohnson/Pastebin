e359549bb953cf6b1621211408959538da541ea4
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-07 16:33:33 +0530

    modedd ui

--- app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -28,8 +27,8 @@ public class AddPaste extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_paste);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +38,7 @@ public class AddPaste extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         llFirstStep = (LinearLayout) findViewById(R.id.llFirstStep);
         ll3rdStep = (LinearLayout) findViewById(R.id.ll3rdStep);
