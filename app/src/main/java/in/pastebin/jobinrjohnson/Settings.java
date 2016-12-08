18a7bd042876bd12b9abe54b679e7793af87aa3e
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-08 13:00:20 +0530

    removed unused

---
+++ app/src/main/java/in/pastebin/jobinrjohnson/Settings.java
@@ -0,0 +1,13 @@
+package in.pastebin.jobinrjohnson;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
