db04ff866dbe3ad5b9ff4ce5d3a0562243f212ec
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-08 07:48:51 +0530

    added profile UI

--- app/src/main/java/in/pastebin/jobinrjohnson/UserHome.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/UserHome.java
@@ -1,30 +1,42 @@
 package in.pastebin.jobinrjohnson;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class UserHome extends AppCompatActivity {
+
+    Button btnLogout, btnNewpaste, btnViewPaste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+        btnNewpaste = (Button) findViewById(R.id.btnNewpaste);
+        btnViewPaste = (Button) findViewById(R.id.btnViewPaste);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 }
