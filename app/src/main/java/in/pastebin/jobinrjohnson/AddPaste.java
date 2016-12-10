eba4f001d90b667d8cebdd41a4e8a4858f1d9887
Jobin Johnson <jobinrjohnson@gmail.com>
2016-12-10 11:10:53 +0530

    added close functionality 1

--- app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
+++ app/src/main/java/in/pastebin/jobinrjohnson/AddPaste.java
@@ -180,7 +180,7 @@ public class AddPaste extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                //navigateStep(false);
+                finish();
                 break;
         }
         return true;
